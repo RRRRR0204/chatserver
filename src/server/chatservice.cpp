985f8558f305c1e84000f217cede24819fddf04d
renzhuoli <1024047967@qq.com>
2023-02-15 22:34:54 +0800

    初步测试

--- src/server/chatservice.cpp
+++ src/server/chatservice.cpp
@@ -38,9 +38,11 @@ MsgHandler ChatService::getHandler(int msgid)
 // 处理登录业务
 void ChatService::login(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
+    LOG_INFO << "do login service!";
 }
 
 // 处理注册业务
 void ChatService::reg(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
+    LOG_INFO << "do reg service!";
 }
